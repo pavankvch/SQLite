fcd7e713c43e1eca3d6124bd214679941602bfb5
pavankvch <pavankvch@gmail.com>
2016-01-08 12:54:41 +0530

    SQLite Tutorial with Login and Registration

---
+++ app/src/main/java/com/visualit/sqlite/Welcome.java
@@ -0,0 +1,14 @@
+package com.visualit.sqlite;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DELL on 12/11/2015.
+ */
+public class Welcome extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
